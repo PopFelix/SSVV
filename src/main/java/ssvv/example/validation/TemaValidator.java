d381e9893586107daa68a9cbe615f28bb68c21a8
916-Pop-Felix <felcris2001@gmail.com>
2023-04-03 21:46:58 +0300

    lab 3 done

--- src/main/java/ssvv/example/validation/TemaValidator.java
+++ src/main/java/ssvv/example/validation/TemaValidator.java
@@ -11,16 +11,16 @@ public class TemaValidator implements Validator<Tema> {
      */
     @Override
     public void validate(Tema entity) throws ValidationException {
-        if(entity.getID().equals("") || entity.getID() == null) {
+        if (entity.getID() == null || entity.getID().equals("")) {
             throw new ValidationException("Numar tema invalid!");
         }
-        if(entity.getDescriere().equals("")){
+        if (entity.getDescriere().equals("")) {
             throw new ValidationException("Descriere invalida!");
         }
-        if(entity.getDeadline() < 1 || entity.getDeadline() > 14) {
+        if (entity.getDeadline() < 1 || entity.getDeadline() > 14) {
             throw new ValidationException("Deadlineul trebuie sa fie intre 1-14.");
         }
-        if(entity.getPrimire() < 1 || entity.getPrimire() > 14) {
+        if (entity.getPrimire() < 1 || entity.getPrimire() > 14) {
             throw new ValidationException("Saptamana primirii trebuie sa fie intre 1-14.");
         }
     }
--- src/test/java/ssvv/example/service/ServiceTest.java
+++ src/test/java/ssvv/example/service/ServiceTest.java
@@ -5,8 +5,11 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ssvv.example.domain.Student;
+import ssvv.example.domain.Tema;
 import ssvv.example.repository.StudentXMLRepo;
+import ssvv.example.repository.TemaXMLRepo;
 import ssvv.example.validation.StudentValidator;
+import ssvv.example.validation.TemaValidator;
 import ssvv.example.validation.ValidationException;
 
 
@@ -16,22 +19,31 @@ class ServiceTest {
     public static final String GRUPA_INCORECTA = "Grupa incorecta!";
     public static final String EMAIL_INCORECT = "Email incorect!";
     private Service service;
-    private StudentXMLRepo repo;
-    private StudentValidator validator;
+    private StudentXMLRepo studentRepo;
+    private StudentValidator studentValidator;
+    private TemaXMLRepo assignmentRepo;
+    private TemaValidator assignmentValidator;
     String filenameStudent = "fisiere/test/Studenti.xml";
+    String filenameAssignment = "fisiere/test/Teme.xml";
     private Student student;
+    private Tema assignment;
 
     @BeforeEach
     public void init() {
-        repo = new StudentXMLRepo(filenameStudent);
-        validator = new StudentValidator();
-        service = new Service(repo, validator, null, null, null, null);
+        studentRepo = new StudentXMLRepo(filenameStudent);
+        studentValidator = new StudentValidator();
+        assignmentRepo = new TemaXMLRepo(filenameAssignment);
+        assignmentValidator = new TemaValidator();
+        service = new Service(studentRepo, studentValidator, assignmentRepo, assignmentValidator, null, null);
         student = new Student("1", "name", 933, "email");
+        assignment = new Tema("1","desc",2,3);
+
     }
 
     @AfterEach
     public void tearDown() {
         service.deleteStudent("1");
+        service.deleteTema("1");
     }
 
     @Test
@@ -54,7 +66,7 @@ class ServiceTest {
     }
 
     /*
-     * In order to check if id is null, we shouldn't call the equals method before the null check in the validator
+     * In order to check if id is null, we shouldn't call the equals method before the null check in the studentValidator
      * Changed order so null check is before empty check
      * */
     @Test
@@ -94,5 +106,51 @@ class ServiceTest {
     void givenEmptyStudentEmail_shouldThrowException() {
         Exception thrown = Assertions.assertThrows(ValidationException.class, () -> service.addStudent(new Student("122", "nume", 123, null)));
         Assertions.assertEquals(EMAIL_INCORECT, thrown.getMessage());
+    }
+
+    @Test
+    void givenValidAssignment_shouldReturnAssignment() {
+        Tema addedAssignment = service.addTema(assignment);
+        Assertions.assertNull(addedAssignment);
+    }
+
+    @Test
+    public void givenValidAssignment_withAssignmentAlreadySavedInMemory_shouldReturnNull() {
+        Tema addedAssignment = service.addTema(assignment);
+        Assertions.assertEquals(assignment, service.addTema(assignment));
+
+    }
+
+    /*
+    * Same thing with the null safe check
+    * */
+    @Test
+    void givenNullAssignmentId_shouldThrowException() {
+        Exception thrown = Assertions.assertThrows(ValidationException.class, () -> service.addTema(new Tema(null, "desc", 1, 3)));
+        Assertions.assertEquals("Numar tema invalid!", thrown.getMessage());
+    }
+
+    @Test
+    void givenEmptyAssignmentId_shouldThrowException() {
+        Exception thrown = Assertions.assertThrows(ValidationException.class, () -> service.addTema(new Tema("", "desc", 1, 3)));
+        Assertions.assertEquals("Numar tema invalid!", thrown.getMessage());
+    }
+
+    @Test
+    void givenEmptyAssignmentDesc_shouldThrowException() {
+        Exception thrown = Assertions.assertThrows(ValidationException.class, () -> service.addTema(new Tema("2", "", 1, 3)));
+        Assertions.assertEquals("Descriere invalida!", thrown.getMessage());
+    }
+
+    @Test
+    void givenInvalidAssignmentDeadline_shouldThrowException() {
+        Exception thrown = Assertions.assertThrows(ValidationException.class, () -> service.addTema(new Tema("2", "desc", 122, 3)));
+        Assertions.assertEquals("Deadlineul trebuie sa fie intre 1-14.", thrown.getMessage());
+    }
+
+    @Test
+    void givenInvalidAssignmentPrimire_shouldThrowException() {
+        Exception thrown = Assertions.assertThrows(ValidationException.class, () -> service.addTema(new Tema("2", "desc", 1, 213)));
+        Assertions.assertEquals("Saptamana primirii trebuie sa fie intre 1-14.", thrown.getMessage());
     }
 }
